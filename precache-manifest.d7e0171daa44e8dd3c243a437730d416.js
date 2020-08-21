15df37a11d4fb1a74f3d515180e00418e7be2b93
lego <lego2nd@gmail.com>
2020-08-21 23:30:36 +0900

    Updates

---
+++ precache-manifest.d7e0171daa44e8dd3c243a437730d416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4448206dfec0f7e9de5fa86412af3f1",
+    "url": "/burger5/index.html"
+  },
+  {
+    "revision": "e69ab88f8a60d99b2929",
+    "url": "/burger5/static/css/main.8686422b.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger5/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger5/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69ab88f8a60d99b2929",
+    "url": "/burger5/static/js/main.d3800cf3.chunk.js"
+  },
+  {
+    "revision": "095325af3e4b6f26c549",
+    "url": "/burger5/static/js/runtime-main.b49785a0.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger5/static/media/burger-logo.b8503d26.png"
+  }
+]);
